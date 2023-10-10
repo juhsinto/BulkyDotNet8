cce513d897ebf7ffda91be53d6b0fcdc32139537
Jacinto Mendes <jacinto.mendes@ciandt.com>
2023-10-10 17:19:19 +1100

    till 2:15:00

--- BulkyWeb/Data/ApplicationDbContext.cs
+++ BulkyWeb/Data/ApplicationDbContext.cs
@@ -12,6 +12,19 @@ namespace BulkyWeb.Data
 		}
 
 		public DbSet<Category> Categories { get; set; }
-	}
+
+
+		// to seed data into db
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //base.OnModelCreating(modelBuilder);
+			modelBuilder.Entity<Category>().HasData(
+					new Category { Id=1, Name="Action", DisplayOrder=1},
+                    new Category { Id = 2, Name = "SciFi", DisplayOrder = 2 },
+                    new Category { Id = 3, Name = "History", DisplayOrder = 3 }
+                );
+        }
+
+    }
 }
 
