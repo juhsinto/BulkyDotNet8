84d8f65d5842d06b9a0f2775f0d201ba012961dc
Jacinto Mendes <jacinto.mendes@ciandt.com>
2023-10-10 12:32:04 +1100

    Test

---
+++ BulkyWeb/Models/Category.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace BulkyWeb.Models
+{
+	public class Category
+	{
+		public int Id { get; set; }
+		public String Name { get; set; }
+		public int DisplayOrder { get; set; }
+	}
+}
+
