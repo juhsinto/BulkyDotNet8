a3ccd81285f2387256be47635017b48d20e82f87
Jacinto Mendes <jacinto.mendes@ciandt.com>
2023-10-13 07:13:54 +1100

    stopped at 3:44:05

---
+++ BulkyWebRazor_Temp/Models/Category.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BulkyWebRazor_Temp.Models
+{
+	public class Category
+	{
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(30)]
+        [DisplayName("Category Name")] // annotation for clientside
+        public required string Name { get; set; }
+        [DisplayName("Display Order")]
+        [Range(1, 100, ErrorMessage = "Display order must be between 1 and 100")] // validation
+        public int DisplayOrder { get; set; }
+    }
+}
+
