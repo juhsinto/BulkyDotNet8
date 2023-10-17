c79278030e6150117a047b7e800d7076ac021472
Jacinto Mendes <jacinto.mendes@ciandt.com>
2023-10-17 14:44:42 +1100

    till 4:13:28 - edit works

--- BulkyWebRazor_Temp/obj/Debug/net8.0/BulkyWebRazor_Temp.AssemblyInfo.cs
+++ BulkyWebRazor_Temp/obj/Debug/net8.0/BulkyWebRazor_Temp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BulkyWebRazor_Temp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3ccd81285f2387256be47635017b48d20e82f87")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c636c8b81116814c8112c1af1782d1a9bbfdc060")]
 [assembly: System.Reflection.AssemblyProductAttribute("BulkyWebRazor_Temp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BulkyWebRazor_Temp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
