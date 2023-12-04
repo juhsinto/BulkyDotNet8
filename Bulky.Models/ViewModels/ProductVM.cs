4a89b0556ea11290298beee0d2d5967128396ef0
Jacinto Mendes <jacinto.mendes@ciandt.com>
2023-12-05 09:02:19 +1100

    till 6:34:03

---
+++ Bulky.Models/ViewModels/ProductVM.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using BulkyWeb.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Bulky.Models.ViewModels
+{
+	public class ProductVM
+	{
+		public Product Product { get; set; }
+        public IEnumerable<SelectListItem> CategoryList { get; set; }
+    }
+}
+
