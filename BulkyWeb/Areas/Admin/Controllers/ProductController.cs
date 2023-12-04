b9c88485227c3cbf866725f95efe64a541582ae1
Jacinto Mendes <jacinto.mendes@ciandt.com>
2023-12-05 07:30:19 +1100

    at 6:30:05 - before viewModel

--- BulkyWeb/Areas/Admin/Controllers/ProductController.cs
+++ BulkyWeb/Areas/Admin/Controllers/ProductController.cs
@@ -39,7 +39,8 @@ namespace BulkyWeb.Areas.Admin.Controllers
                     Text = u.Name,
                     Value = u.Id.ToString()
                 });
-            ViewBag.CategoryList = CategoryList;
+            //ViewBag.CategoryList = CategoryList;
+            ViewData["CategoryList"] = CategoryList;
 
             return View();
         }
