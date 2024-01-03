2e8ee451774de3a31ab1b1f15c04e2fe78cc925e
Jacinto Mendes <jacinto.mendes@ciandt.com>
2024-01-03 20:39:15 +1100

    till 7:29:45

--- BulkyWeb/Areas/Admin/Controllers/ProductController.cs
+++ BulkyWeb/Areas/Admin/Controllers/ProductController.cs
@@ -195,6 +195,15 @@ namespace BulkyWeb.Areas.Admin.Controllers
             TempData["success"] = "Product deleted successfully";
             return RedirectToAction("Index");
         }
+
+        #region API CALLS
+        public IActionResult GetAll()
+        {
+            List<Product> objProductList = _unitOfWork.Product.GetAll(includeProperties: "Category").ToList();
+            return Json(new { data = objProductList });
+        }
+        
+        #endregion
     }
 }
 
